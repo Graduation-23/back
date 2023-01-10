000e68179dbded658dd025fe6dd3ebfd4257b4d9
Bonwoong <thesmartist@naver.com>
2023-01-10 18:24:01 +0900

    init

---
+++ src/main/java/com/graduation23/accountdiary/AccountdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.graduation23.accountdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountdiaryApplication.class, args);
+	}
+
+}
