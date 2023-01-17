4c9449f04421b65ef9a483d657b6a8b44b389cf3
min-0 <dhe08133@naver.com>
2023-01-18 00:07:14 +0900

    chore: Create Project

--- src/test/java/graduation/spendiary/SpendiaryApplicationTests.java
+++ src/test/java/graduation/spendiary/SpendiaryApplicationTests.java
@@ -1,13 +1,13 @@
-package com.graduation23.accountdiary;
+package graduation.spendiary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AccountdiaryApplicationTests {
+class SpendiaryApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
