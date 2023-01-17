c13aa5ce0991c55937a01bf2a8146049f5d29346
byungjin <byungjin.dev@gmail.com>
2023-01-17 20:14:19 +0900

    fix: Fix Jenkins Application Test properties Error

--- src/test/java/com/graduation23/accountdiary/AccountdiaryApplicationTests.java
+++ src/test/java/com/graduation23/accountdiary/AccountdiaryApplicationTests.java
@@ -3,7 +3,7 @@ package com.graduation23.accountdiary;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AccountdiaryApplicationTests {
 
 	@Test
