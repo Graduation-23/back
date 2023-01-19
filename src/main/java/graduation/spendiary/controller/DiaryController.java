7f6b25b2c895217286acd3bf5c48b2c65635ff2b
min-0 <dhe08133@naver.com>
2023-01-19 20:39:30 +0900

    feat: 다이어리 생성 및 조회 testAPI

--- src/main/java/graduation/spendiary/controller/DiaryController.java
+++ src/main/java/graduation/spendiary/controller/DiaryController.java
@@ -1,8 +1,13 @@
 package graduation.spendiary.controller;
 
+import graduation.spendiary.domain.diary.Diary;
 import graduation.spendiary.domain.diary.DiaryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/test/diarys")
@@ -12,17 +17,22 @@ public class DiaryController {
     private final DiaryRepository diaryRepository;
 
     @GetMapping
-    public String diarys() {
-        return "GET diarys";
-    }
-
-    @PostMapping
-    public String  createDiary() {
-        return "POST diary";
+    public String diarys(Model model) {
+        List<Diary> diarys = diaryRepository.findAll();
+        model.addAttribute("diarys", diarys);
+        return "전체 조회";
     }
 
     @GetMapping("/{diaryId}")
-    public String findDiary(@PathVariable String diaryId) {
-        return "GET diaryId = " + diaryId;
+    public String  diary(@PathVariable Long diaryId, Model model) {
+        Diary diary = diaryRepository.findById(diaryId);
+        model.addAttribute("diary", diary);
+        return "상세 조회";
+    }
+
+    @PostMapping("/add")
+    public String addDiary(Diary diary) {
+        diaryRepository.save(diary);
+        return "생성";
     }
 }
