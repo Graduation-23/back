d60edd20af9011804a2addf4590cdaea34226d6e
byungjin <byungjin.dev@gmail.com>
2023-02-03 20:34:22 +0900

    chore: Remove useless module import statment

--- src/main/java/graduation/spendiary/controller/AuthController.java
+++ src/main/java/graduation/spendiary/controller/AuthController.java
@@ -14,8 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
