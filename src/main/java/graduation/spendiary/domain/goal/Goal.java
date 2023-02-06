7b3c22c2e8ad380ac486e2eb8c6e53100a0596fa
DoubleDeltas <thesmartist@naver.com>
2023-02-06 19:46:09 +0900

    feat: goal entity, repo, service

---
+++ src/main/java/graduation/spendiary/domain/goal/Goal.java
@@ -0,0 +1,32 @@
+package graduation.spendiary.domain.goal;
+
+import lombok.*;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "Goal")
+@AllArgsConstructor
+@Data
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Goal {
+
+    @Transient
+    public static final String SEQUENCE_NAME = "goal_sequence";
+
+    @Field("goal_id")
+    private Long id;
+
+    @Field("user_id")
+    private String user;
+
+    @Field("goal_name")
+    private String name;
+
+    @Field("goal_amount")
+    private Long amount;
+
+    @Field("goal_state")
+    private String state;
+}
