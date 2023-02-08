85f43f8c5cbecb9cf96cbee4e2712b58f2a3f130
DoubleDeltas <thesmartist@naver.com>
2023-02-09 08:50:17 +0900

    refactor: '/api/diarys' -> 'api/diary'

--- src/main/java/graduation/spendiary/controller/DiaryController.java
+++ src/main/java/graduation/spendiary/controller/DiaryController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/diarys")
+@RequestMapping("/api/diary")
 @RequiredArgsConstructor
 public class DiaryController {
 
