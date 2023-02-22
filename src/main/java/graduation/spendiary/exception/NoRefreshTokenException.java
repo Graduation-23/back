22ea9947d9954b0c972c4861cdcc7fbb571dcf3c
DoubleDeltas <thesmartist@naver.com>
2023-02-22 15:53:40 +0900

    fix: exception messages and http codes

--- src/main/java/graduation/spendiary/exception/NoRefreshTokenException.java
+++ src/main/java/graduation/spendiary/exception/NoRefreshTokenException.java
@@ -3,6 +3,6 @@ package graduation.spendiary.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.FORBIDDEN, reason = "다이어리를 더 이상 수정할 수 없음")
+@ResponseStatus(code = HttpStatus.GONE, reason = "Refresh Token 없음")
 public class NoRefreshTokenException extends RuntimeException{
 }
