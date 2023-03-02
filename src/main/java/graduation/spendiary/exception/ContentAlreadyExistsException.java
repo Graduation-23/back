bdb7f9c424136383011a61d193f2daa1506ea45e
DoubleDeltas <thesmartist@naver.com>
2023-03-02 15:48:41 +0900

    refactor: GoalService exception handling

--- src/main/java/graduation/spendiary/exception/ContentAlreadyExistsException.java
+++ src/main/java/graduation/spendiary/exception/ContentAlreadyExistsException.java
@@ -4,5 +4,5 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.FORBIDDEN, reason = "날짜가 중복되는 다이어리를 생성할 수 없음")
-public class DiaryDuplicatedException extends RuntimeException{
+public class ContentAlreadyExistsException extends RuntimeException{
 }
